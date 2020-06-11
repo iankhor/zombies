2eec098069702e84371add38eebd4fc474c81f14
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-06-12 08:51:25 +1000

    NormaliseCoordinates specas

--- src/lib/grid.test.ts
+++ src/lib/grid.test.ts
@@ -5,128 +5,170 @@ import {
 	removeEntity,
 	moveEntity,
 	calculateDirectionMagnitude,
-} from "lib/grid";
+	normaliseCoordinate,
+} from 'lib/grid'
 
-describe("createGrid", () => {
-	it("is a square grid", () => {
-		const grid = createGrid(3);
+describe('createGrid', () => {
+	const grid = createGrid(2)
+	it('is a square grid', () => {
+		expect(grid[0]).toHaveLength(2)
+		expect(grid).toHaveLength(2)
+	})
 
-		expect(grid[0]).toHaveLength(3);
-		expect(grid).toHaveLength(3);
-	});
+	it('has the entities in each grid', () => {
+		expect(grid[0][0]).toEqual({ entities: [] })
+		expect(grid[0][1]).toEqual({ entities: [] })
+		expect(grid[1][0]).toEqual({ entities: [] })
+		expect(grid[1][1]).toEqual({ entities: [] })
+	})
+})
 
-	it("has the entities in each grid", () => {
-		const grid = createGrid(2);
+describe('addEntity', () => {
+	const grid = createGrid(4)
 
-		expect(grid[0][0]).toEqual({ entities: [] });
-		expect(grid[0][1]).toEqual({ entities: [] });
-		expect(grid[1][0]).toEqual({ entities: [] });
-		expect(grid[1][1]).toEqual({ entities: [] });
-	});
-});
+	it('adds an entity to grid based on coordinates', () => {
+		const entity = { id: 2, type: 'zombie' }
+		const updatedGrid = addEntity(grid, 1, 3, entity)
 
-describe("addEntity", () => {
-	it("adds an entity to grid based on coordinates", () => {
-		const grid = createGrid(4);
-		const entity = { id: 2, type: "zombie" };
+		expect(updatedGrid[1][3]).toMatchObject({ entities: [entity] })
+	})
 
-		const updatedGrid = addEntity(grid, 1, 3, entity);
+	it('does not override existing grid infomation', () => {
+		const entity = { id: 3, type: 'zombie' }
+		grid[0][0] = { entities: [{ id: 2, type: 'creature' }] }
 
-		expect(updatedGrid[1][3]).toMatchObject({ entities: [entity] });
-	});
-
-	it("does not override existing grid infomation", () => {
-		const grid = createGrid(5);
-		const entity = { id: 3, type: "zombie" };
-		grid[0][0] = { entities: [{ id: 2, type: "creature" }] };
-
-		const updatedGrid = addEntity(grid, 0, 0, entity);
+		const updatedGrid = addEntity(grid, 0, 0, entity)
 
 		expect(updatedGrid[0][0].entities).toEqual(
 			expect.arrayContaining([
-				{ id: 3, type: "zombie" },
-				{ id: 2, type: "creature" },
+				{ id: 3, type: 'zombie' },
+				{ id: 2, type: 'creature' },
 			])
-		);
-	});
+		)
+	})
 
-	it("throws an error if given invalid coordinates", () => {
-		const grid = createGrid(4);
-		const entity = { id: 3, type: "zombie" };
+	it('throws an error if given invalid coordinates', () => {
+		const grid = createGrid(4)
+		const entity = { id: 3, type: 'zombie' }
 
 		expect(() => addEntity(grid, -1, 3, entity)).toThrow(
-			"Invalid grid coordinates !"
-		);
+			'Invalid grid coordinates !'
+		)
 		expect(() => addEntity(grid, 0, -1, entity)).toThrow(
-			"Invalid grid coordinates !"
-		);
+			'Invalid grid coordinates !'
+		)
 		expect(() => addEntity(grid, 0, 4, entity)).toThrow(
-			"Invalid grid coordinates !"
-		);
-	});
-});
+			'Invalid grid coordinates !'
+		)
+	})
+})
 
-describe("findEntity", () => {
-	it("returns coordinates of entity", () => {
-		const grid = createGrid(5);
-		const entity = { id: 2, type: "zombie" };
+describe('findEntity', () => {
+	const grid = createGrid(5)
+	const entity = { id: 2, type: 'zombie' }
 
-		const updatedGrid = addEntity(grid, 3, 4, entity);
+	it('returns coordinates of entity', () => {
+		const updatedGrid = addEntity(grid, 3, 4, entity)
 
 		expect(findEntity(updatedGrid, entity.id)).toEqual({
 			coordinates: { x: 3, y: 4 },
 			entity,
-		});
-	});
-});
+		})
+	})
+})
 
-describe("removeEntity", () => {
-	it("removes an entity to grid based on coordinates", () => {
-		const grid = createGrid(4);
+describe('removeEntity', () => {
+	it('removes an entity to grid based on coordinates', () => {
+		const grid = createGrid(4)
 
-		grid[3][1].entities = [{ id: 9, type: "zombie" }];
+		grid[3][1].entities = [{ id: 9, type: 'zombie' }]
 
-		const updatedGrid = removeEntity(grid, 3, 1, 9);
-		expect(updatedGrid[3][1].entities).toEqual([]);
-	});
+		const updatedGrid = removeEntity(grid, 3, 1, 9)
+		expect(updatedGrid[3][1].entities).toEqual([])
+	})
 
-	it("removes an entity that does not exist", () => {
-		const grid = createGrid(4);
+	it('removes an entity that does not exist', () => {
+		const grid = createGrid(4)
 
-		grid[3][1].entities = [{ id: 9, type: "zombie" }];
+		grid[3][1].entities = [{ id: 9, type: 'zombie' }]
 
-		const updatedGrid = removeEntity(grid, 3, 1, 123);
-		expect(updatedGrid[3][1].entities).toEqual([{ id: 9, type: "zombie" }]);
-	});
-});
+		const updatedGrid = removeEntity(grid, 3, 1, 123)
+		expect(updatedGrid[3][1].entities).toEqual([{ id: 9, type: 'zombie' }])
+	})
+})
 
-describe("moveEntity", () => {
-	it("moves to its new position on grid", () => {
-		const grid = createGrid(2);
-		const entity = { id: 2, type: "creature" };
-		grid[0][0].entities = [entity];
+describe('moveEntity', () => {
+	const entity = { id: 2, type: 'creature' }
+	const grid = createGrid(2)
 
-		const updatedGrid = moveEntity(grid, 2, "D");
+	it('moves to its new position on grid', () => {
+		grid[0][0].entities = [entity]
 
-		expect(updatedGrid[0][0].entities).toEqual([]);
-		expect(updatedGrid[0][1].entities).toEqual([entity]);
-	});
-});
+		const updatedGrid = moveEntity(grid, 2, 'D')
 
-describe("calculateDirectionMagnitude", () => {
+		expect(updatedGrid[0][0].entities).toEqual([])
+		expect(updatedGrid[0][1].entities).toEqual([entity])
+	})
+
+	it('its horizontal movement is circular', () => {
+		grid[0][1].entities = [entity]
+
+		const updatedGrid = moveEntity(grid, 2, 'D')
+
+		expect(updatedGrid[0][1].entities).toEqual([])
+		expect(updatedGrid[0][0].entities).toEqual([entity])
+	})
+
+	it('its vertical movement is circular', () => {
+		grid[0][0].entities = [entity]
+
+		const updatedGrid = moveEntity(grid, 2, 'L')
+
+		expect(updatedGrid[0][0].entities).toEqual([])
+		expect(updatedGrid[1][0].entities).toEqual([entity])
+	})
+})
+
+describe('calculateDirectionMagnitude', () => {
 	test.each`
 		direction   | magnitudeCoordinates
-		${"U"}      | ${{ x: 0, y: -1 }}
-		${"D"}      | ${{ x: 0, y: 1 }}
-		${"L"}      | ${{ x: -1, y: 0 }}
-		${"R"}      | ${{ x: 1, y: 0 }}
-		${"foobaz"} | ${{ x: 0, y: 0 }}
+		${'U'}      | ${{ x: 0, y: -1 }}
+		${'D'}      | ${{ x: 0, y: 1 }}
+		${'L'}      | ${{ x: -1, y: 0 }}
+		${'R'}      | ${{ x: 1, y: 0 }}
+		${'foobaz'} | ${{ x: 0, y: 0 }}
 	`(
-		"return $magnitudeCoordinates when given a direction of $direction",
+		'return $magnitudeCoordinates when given a direction of $direction',
 		({ direction, magnitudeCoordinates }) => {
 			expect(calculateDirectionMagnitude(direction)).toMatchObject(
 				magnitudeCoordinates
-			);
+			)
 		}
-	);
-});
+	)
+})
+
+describe('normaliseCoordinate', () => {
+	const grid = createGrid(3)
+
+	test.each`
+		movementMagnitude | normalisedMovementMagnitude
+		${-5}             | ${1}
+		${-4}             | ${2}
+		${-3}             | ${0}
+		${-2}             | ${1}
+		${-1}             | ${2}
+		${0}              | ${0}
+		${1}              | ${1}
+		${2}              | ${2}
+		${3}              | ${0}
+		${4}              | ${1}
+		${5}              | ${2}
+	`(
+		'return $normalisedMovementMagnitude when given a magnitude of $movementMagnitude for grid size 3',
+		({ movementMagnitude, normalisedMovementMagnitude }) => {
+			expect(normaliseCoordinate(grid.length, movementMagnitude)).toEqual(
+				normalisedMovementMagnitude
+			)
+		}
+	)
+})
--- src/lib/grid.ts
+++ src/lib/grid.ts
@@ -1,97 +1,120 @@
 type Entity = {
-  id: number
-  type: string
+	id: number
+	type: string
 }
 
 type GridInfo = {
-  entities:  Entity[]
+	entities: Entity[]
 }
 
 type Coordinates = {
-  x: number
-  y: number
+	x: number
+	y: number
 }
 
 type FoundEntity = {
-  coordinates: Coordinates
-  entity: Entity
+	coordinates: Coordinates
+	entity: Entity
 }
 
 export const createGrid = (size: number): GridInfo[][] => {
-  const tempGrid = [] as GridInfo[][]
-  let x, y
+	const tempGrid = [] as GridInfo[][]
+	let x, y
 
-  for (x = 0; x < size; x++) {
-    let tempRow = [] as GridInfo[]
-    
-    for(y = 0; y < size; y++) tempRow.push({ entities: [] })
-    tempGrid.push(tempRow)
-  }
+	for (x = 0; x < size; x++) {
+		let tempRow = [] as GridInfo[]
 
-  return tempGrid
+		for (y = 0; y < size; y++) tempRow.push({ entities: [] })
+		tempGrid.push(tempRow)
+	}
+
+	return tempGrid
 }
 
-export const addEntity = (grid: GridInfo[][], x: number, y: number, entity: Entity): GridInfo[][] => {
-  try {
-    grid[x][y] = { 
-      ...grid[x][y], 
-      entities: [...grid[x][y].entities, entity] 
-    }
-  } catch (e) {
-    throw 'Invalid grid coordinates !'
-  }
-  
-  
-  return grid
+export const addEntity = (
+	grid: GridInfo[][],
+	x: number,
+	y: number,
+	entity: Entity
+): GridInfo[][] => {
+	try {
+		grid[x][y] = {
+			...grid[x][y],
+			entities: [...grid[x][y].entities, entity],
+		}
+	} catch (e) {
+		throw 'Invalid grid coordinates !'
+	}
+
+	return grid
 }
 
 export const findEntity = (grid: GridInfo[][], id: number): any => {
-  let x: number, y: number
-  let coordinates = {} as Coordinates
-  let foundEntity = {} as Entity
-  
+	let x: number, y: number
+	let coordinates = {} as Coordinates
+	let foundEntity = {} as Entity
 
-  for (x = 0; x < grid.length; x++) {
-    for (y = 0; y < grid.length; y++) {
-      const entity = grid[x][y].entities.find(e => e.id === id)
-      
-      if (entity) {
-        coordinates = { x, y }
-        foundEntity = entity
-      }
-    }
-  }
+	for (x = 0; x < grid.length; x++) {
+		for (y = 0; y < grid.length; y++) {
+			const entity = grid[x][y].entities.find((e) => e.id === id)
 
-  return { coordinates, entity: foundEntity }
+			if (entity) {
+				coordinates = { x, y }
+				foundEntity = entity
+			}
+		}
+	}
+
+	return { coordinates, entity: foundEntity }
 }
 
-export const removeEntity = (grid: GridInfo[][], x: number, y: number, entityId: number): GridInfo[][] => {
-  grid[x][y].entities = grid[x][y].entities.filter(e => e.id !== entityId)
+export const removeEntity = (
+	grid: GridInfo[][],
+	x: number,
+	y: number,
+	entityId: number
+): GridInfo[][] => {
+	grid[x][y].entities = grid[x][y].entities.filter((e) => e.id !== entityId)
 
-  return grid
+	return grid
 }
 
-export const moveEntity = (grid: GridInfo[][], entityId: number, direction: string): GridInfo[][] => {
-  const { coordinates: { x: currentX, y: currentY }, entity } = findEntity(grid, entityId)
-  const { x: moveX , y: moveY } = calculateDirectionMagnitude(direction)
-  const removedEntityGrid = removeEntity(grid, currentX, currentY, entityId)
+export const moveEntity = (
+	grid: GridInfo[][],
+	entityId: number,
+	direction: string
+): GridInfo[][] => {
+	const {
+		coordinates: { x: currentX, y: currentY },
+		entity,
+	} = findEntity(grid, entityId)
+	const { x: moveX, y: moveY } = calculateDirectionMagnitude(direction)
+	const removedEntityGrid = removeEntity(grid, currentX, currentY, entityId)
 
-  return addEntity(removedEntityGrid, currentX + moveX, currentY + moveY, entity)
-} 
+	return addEntity(
+		removedEntityGrid,
+		normaliseCoordinate(grid.length, currentX + moveX),
+		normaliseCoordinate(grid.length, currentY + moveY),
+		entity
+	)
+}
+
+export const normaliseCoordinate = (gridSize: number, gridPosition: number) =>
+	((gridPosition % gridSize) + gridSize) % gridSize //handling negative numbers for JS modolo bug
 
 export const calculateDirectionMagnitude = (direction: string): Coordinates => {
-  switch (direction) {
-    case 'U':
-      return { x: 0, y: -1 }
-    case 'D':
-      return { x: 0, y: 1 }
-    case 'L':
-      return { x: -1, y: 0 }
-    case 'R':
-      return { x: 1, y: 0 }
-    default: 
-      return { x: 0, y: 0 }
-  }
+	switch (direction) {
+		case 'U':
+			return { x: 0, y: -1 }
+		case 'D':
+			return { x: 0, y: 1 }
+		case 'L':
+			return { x: -1, y: 0 }
+		case 'R':
+			return { x: 1, y: 0 }
+		default:
+			return { x: 0, y: 0 }
+	}
 }
 
 export default {}
