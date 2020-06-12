aee1a5ad64c8996fef2be3c0a702f35954683da4
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-06-12 13:22:23 +1000

    Code documentation

--- src/lib/grid.ts
+++ src/lib/grid.ts
@@ -17,6 +17,17 @@ type FoundEntity = {
 	entity: Entity
 }
 
+/**
+ * @param size dimensions for a square grid
+ * @returns a size by size (ie: 2 by 2) dimensional array which will represent
+ *          the x and y coordinates of the grid.
+ *
+ *  ```
+ *  const grid = createGrid(2)
+ *
+ *  grid[1][3] // represent grid information for coordinate x of 1 and coordinates y of 3
+ *  ```
+ */
 export const createGrid = (size: number): GridInfo[][] => {
 	const tempGrid = [] as GridInfo[][]
 	let x, y
@@ -31,6 +42,13 @@ export const createGrid = (size: number): GridInfo[][] => {
 	return tempGrid
 }
 
+/**
+ * @param grid the square grid
+ * @param x horizontal coordinates of the grid
+ * @param y vertical coordinates of the grid
+ * @param entity the entity to placed in coordinates x and y in grid
+ * @returns an updated grid with the entity placed in desired coordinates
+ */
 export const addEntity = (
 	grid: GridInfo[][],
 	x: number,
@@ -49,14 +67,19 @@ export const addEntity = (
 	return grid
 }
 
-export const findEntity = (grid: GridInfo[][], id: number): any => {
+/**
+ * @param grid the square grid
+ * @param entityId the entity entityId that is located in the provided grid
+ * @return returns information about the entity and its location
+ */
+export const findEntity = (grid: GridInfo[][], entityId: number): any => {
 	let x: number, y: number
 	let coordinates = {} as Coordinates
 	let foundEntity = {} as Entity
 
 	for (x = 0; x < grid.length; x++) {
 		for (y = 0; y < grid.length; y++) {
-			const entity = grid[x][y].entities.find((e) => e.id === id)
+			const entity = grid[x][y].entities.find((e) => e.id === entityId)
 
 			if (entity) {
 				coordinates = { x, y }
@@ -68,6 +91,12 @@ export const findEntity = (grid: GridInfo[][], id: number): any => {
 	return { coordinates, entity: foundEntity }
 }
 
+/**
+ * @param grid a square grid
+ * @param x horizontal coordinates of the grid
+ * @param y vertical coordinates of the grid
+ * @param entityId the entity id that is located in the provided grid
+ */
 export const removeEntity = (
 	grid: GridInfo[][],
 	x: number,
@@ -79,6 +108,14 @@ export const removeEntity = (
 	return grid
 }
 
+/**
+ * @param grid a square grid
+ * @param entityId the entity id that is located in the provided grid
+ * @param direction the direction entity to move towards with
+ *                  'L' as left ,
+ *                  'R' as right,
+ *                  'D' as down, 'U' as up
+ */
 export const moveEntity = (
 	grid: GridInfo[][],
 	entityId: number,
@@ -99,9 +136,18 @@ export const moveEntity = (
 	)
 }
 
+/**
+ * @param gridSize size of square grid
+ * @param gridPosition position in grid
+ * @returns circular number within the square grid
+ */
 export const normaliseCoordinate = (gridSize: number, gridPosition: number) =>
 	((gridPosition % gridSize) + gridSize) % gridSize //handling negative numbers for JS modolo bug
 
+/**
+ * @param direction direction of travel
+ * @returns magitude of travel of direction in grid
+ */
 export const calculateDirectionMagnitude = (direction: string): Coordinates => {
 	switch (direction) {
 		case 'U':
